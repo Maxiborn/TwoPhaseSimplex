91764e0bd02d4118e7bbf2db63e936e367ebfcb9
Maximilian Born <maximilianborn@Maximilians-MBP.fritz.box>
2020-09-17 10:53:10 +0200

    Rounding example with wrong solution

--- Example_round_w.py
+++ Example_round_w.py
@@ -5,11 +5,10 @@ if __name__ == "__main__":
 
     #Add Input in Standardform here:
     
-    c = [-0.5, -3, 0, 0, 0]                 #unlimited LP
-    b = [6, 2, 8]                        
-    A = [[0, 1, 1, 0, 0],
-         [1, 0, 0, -1, 0],
-         [1, 2, 0, 0, -1]]
+    c = [-2.0000000000000001, -1, 0, 0]  #rounding example (wrong solution)
+    b = [1, 3]                        
+    A = [[0, 1, 1, 0],
+         [2, 1, 0, 1]]
     
     algo = TwoPhaseSimplex()
     z, b_hat, indices, delta_b, delta_c_B, reduced_costs, shadow_prices = algo.two_phase_simplex(c, b, A)
@@ -31,7 +30,18 @@ if __name__ == "__main__":
     """
     Output:
 
-    LP is unlimited.
+
+    Results:
+    z: -3.0
+    Indices: [2 1 3 4]
+    x_B: [1. 1.]           # wrong basisvalues
+
+    Sensitivity:
+
+    Reduced costs: [0. 1.]
+    Delta c: [(-inf, inf), (-inf, 2.0)]
+    Delta b: [(-1.0, 2.0), (-2.0, inf)]
+    Shadow prices: [0.0, 0.0]
     """
 
 
